1ca611972a1830a58c06acc2c9672e25e5ea9f53
FeetCodingHommy <hommyfromkorea@gmail.com>
2021-08-13 04:10:25 +0900

    revert middlewares.py
    이전 예시 코드로 변경

--- melon_scrapy/melon_scrapy/middlewares.py
+++ melon_scrapy/melon_scrapy/middlewares.py
@@ -85,113 +85,27 @@ class MelonScrapyDownloaderMiddleware:
         return middleware
 
     def process_request(self, request, spider):
-        global SUCCEEDED, FAILED, SUCCEEDED_DIR, FAILED_DIR, SONG_DF # 점점 막장이 되어가는 코드
-
         # Called for each request that goes through the downloader
         # middleware.
-        if "&WHYISNTMYCODEWORKING=" in request.url: # 여기에 들어가야 정상
-            new_url, kakaoId = request.url.split("&WHYISNTMYCODEWORKING=")
-            request = request.replace(url=new_url)
-        albumId = new_url.split("?albumId=")[-1]
-        albumId, kakaoId = int(albumId), int(kakaoId) #MY_STRING_CLEANER
 
-        songName = SONG_DF.loc[(SONG_DF["album_id"]==albumId)&(SONG_DF["id"]==kakaoId), "song_name"].values[0]
-        songName = MY_STRING_CLEANER(songName)
-
-        try:
-            assert songName is not None, "WHY IS THIS HAPPENING TO ME?"
-        except AssertionError:
-            print("WHY IS THIS HAPPENING TO ME?")
-            raise Exception("WHY IS THIS HAPPENING TO ME?")
-
-        try:
-            assert "&WHYISNTMYCODEWORKING=" not in request.url, "WHY ISNT MY CODE WORKING?"
-        except AssertionError:
-            print("WHY ISNT MY CODE WORKING?")
-            raise Exception("WHY ISNT MY CODE WORKING?")
-        
         self.driver.get(request.url)
-        # try:
-        #     self.driver.get(request.url)
-        # except:
-        #     sleep(3)
-        #     FAILED.append({"album_id": albumId, "id": kakaoId, "song_name": songName})
-        #     return HtmlResponse(url=request.url, status=404)
 
         melon_album_song_table = self.driver.find_element_by_xpath(
             '//*[@id="frm"]/div/table'
         )
 
         # body = to_bytes(text=self.driver.page_source)
-        temp_file = f"/content/melon_scrapy/my_temp/temp_html_{kakaoId}.html"
-        with open(temp_file, 'w', encoding="utf-8") as html_file:
+        with open("/content/melon_scrapy/temp_html.html", 'w', encoding="utf-8") as html_file:
             html_file.write(melon_album_song_table.get_attribute("outerHTML"))
-        del html_file
-
-        sleep(3)
-
-        # print(f"{kakaoId:>8}", end="    ")
-
-        with open(temp_file, 'r', encoding="utf-8") as html_file:
-            div_table = Selector(text=html_file.read()).xpath('/html/body/table')
-        
-        songs = div_table.xpath('tbody/tr/td[4]/div/div/div[1]/span/a/text()').getall()
-
-        is_song_found = False
-        for i, other_song_name in enumerate(songs):
-            if other_song_name.strip() == songName:
-                song_jshref = div_table.xpath(f'tbody/tr[{i+1}]/td[4]/div/div/div[1]/span/a/@href').extract_first()
-                is_song_found = True
-                break
-        if not is_song_found:
-            for i, other_song_name in enumerate(songs):
-                if ( (other_song_name.strip().replace(' ', '') in songName.replace(' ', ''))
-                    or (songName.replace(' ', '') in other_song_name.strip().replace(' ', '')) ):
-                    song_jshref = div_table.xpath(f'tbody/tr[{i+1}]/td[4]/div/div/div[1]/span/a/@href').extract_first()
-                    is_song_found = True
-                    break
-        
-        if is_song_found:
-            match_obj = re.match(MELON_JSHREF_REGEX, song_jshref)
-            songId = match_obj.group("songId")
-            # print(f"{kakaoId:>8}", end="    ")
-            # print(f"{songName}: https://www.melon.com/song/detail.htm?songId={songId}")
-
-            SUCCEEDED.append({"album_id": albumId, "id": kakaoId, "song_id": songId})
-
-        else:
-            print(f"{kakaoId:>8}", end="    ")
-            print(f"{songName}: NOT FOUND - https://www.melon.com/album/detail.htm?albumId={albumId}")
-            FAILED.append({"album_id": albumId, "id": kakaoId, "song_name": songName})
-        
-        current_counter = len(SUCCEEDED) + len(FAILED)
-        if current_counter >= MY_SAVE_POINT_LENGTH:
-            if not os.path.exists(SUCCEEDED_DIR):
-                pd.DataFrame(SUCCEEDED).to_csv(SUCCEEDED_DIR, mode='w', index=False)
-            else:
-                pd.DataFrame(SUCCEEDED).to_csv(SUCCEEDED_DIR, mode='a', index=False, header=False)
-
-            if not os.path.exists(FAILED_DIR):
-                pd.DataFrame(FAILED).to_csv(FAILED_DIR, mode='w', index=False)
-            else:
-                pd.DataFrame(FAILED).to_csv(FAILED_DIR, mode='a', index=False, header=False)
-
-            print(f"\nhit {MY_SAVE_POINT_LENGTH}. update. : success - {len(SUCCEEDED)} / fail - {len(FAILED)}\n")
-
-            # 초기화
-            SUCCEEDED = list()
-            FAILED = list()
-        
-        if os.path.exists(temp_file):
-            os.remove(temp_file)
+        sleep(5)
 
         # Must either:
         # - return None: continue processing this request
         # - or return a Response object
-        return HtmlResponse(url=request.url)
         # - or return a Request object
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
+        return None
 
     def process_response(self, request, response, spider):
         # Called with the response returned from the downloader.
